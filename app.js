585e32f1b79a4b80bfaa1f9646d903cc79c94496
Andrew TOwnsley <atownsley86@gmail.com>
2021-08-11 22:19:24 -0600

    Mobile menu animation now displays a close X when open, and closes when the  is clicked.

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const burgerBtn = document.getElementById("burgerBtn");
 const mobileNav = document.getElementById("mobileNav");
 const overlay = document.getElementById("overlay");
+const closeX = document.getElementById("closeX");
 
 // Toggle Mobile Menu
 const toggleMobileMenu = () => {
     mobileNav.classList.toggle('active');
     overlay.classList.toggle('active');
+    burgerBtn.classList.toggle('active')
+    closeX.classList.toggle('active')
 }
 
 const closeMobileMenu = () => {
     mobileNav.classList.toggle('active');
     overlay.classList.toggle('active');
+    burgerBtn.classList.toggle('active')
+    closeX.classList.toggle('active')
 }
 
-
+closeX.addEventListener('click', closeMobileMenu);
 burgerBtn.addEventListener('click', toggleMobileMenu);
 overlay.addEventListener('click', closeMobileMenu);
