42d0a5e1113f5e98a2352c35130978c32886d08d
Andrew TOwnsley <atownsley86@gmail.com>
2021-09-04 18:30:02 -0600

    still struggling with the pledge-select activation and radio button selection.  Starting over with the function may be the next best step.

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const openMobile = document.getElementById("openMobile");
 const closeMobile = document.getElementById("closeMobile");
 const projectBtn = document.getElementById("projectBtn");
 const rewardBtn = document.querySelectorAll(".reward-btn");
-console.log(rewardBtn);
 const pledgeModal = document.getElementById("pledgeModal");
 const closePledgeModal = document.getElementById("closePledgeModal");
 const pledgeSelects= document.querySelectorAll(".pledge-select");
-const radioLabel = document.getElementsByClassName("radio-label");
 const radioCircle = document.getElementById("radioCircle");
+const radioBtn = document.querySelectorAll('.radio-label-btn');
 
 
 // Toggle Mobile Menu
@@ -38,22 +37,42 @@ const rewardModalToggle = (e) => {
     }
 }
 
-
 const selectPledge = (e) => {
+    e.preventDefault();
+    const currentPledge = document.querySelector('.pledge-select');
+    const radioLabel = document.querySelector(".radio-label");
     if(e.target !== e.currentTarget) {
-        const currentPledge = document.querySelector('.pledge-select.active');
+        console.log(e.target.parentElement.parentElement.parentElement);
         if(currentPledge) {
-            currentPledge.classList.remove('active');
+            currentPledge.classList.toggle('active');
         }
-        const radio = e.target;
-        radio.parentElement.parentElement.classList.toggle('active');
-        const pledgePopOut = document.querySelector('.pledge-select.active .pledge-pop-out');
-        console.log(pledgePopOut);
+        // const radio = e.target;
+        radioLabel.parentElement.parentElement.parentElement.classList.toggle('active');
+        const pledgePopOut = document.querySelector('.pledge-select .pledge-pop-out');
         pledgePopOut.classList.toggle('active');
     }
     e.stopPropagation;
 }
 
+// const selectPledge = (e) => {
+//     e.preventDefault();
+//     console.log(e.target.parentElement.parentElement.parentElement);
+//     const activePledge = document.querySelector('.pledge-select .active')
+//     const pledge = document.querySelector('.pledge-select')
+//     if(e.target.parentElement.parentElement === activePledge){
+//         console.log("active-removed");
+//         activePledge.classList.remove('active');
+//     } else if
+//     (e.target.parentElement.parentElement.parentElement === pledge) {
+//         console.log("else if");
+//         pledge.classList.toggle('active');
+// }
+// }
+
+
+radioBtn.forEach(pledge => {
+    pledge.addEventListener('click', selectPledge)
+})
 
 pledgeModal.addEventListener('click', selectPledge, false); 
 closePledgeModal.addEventListener('click', togglePledgeModal);
@@ -62,3 +81,20 @@ about.addEventListener('click', rewardModalToggle);
 overlay.addEventListener('click', togglePledgeModal);
 closeMobile.addEventListener('click', toggleMobileMenu);
 openMobile.addEventListener('click', toggleMobileMenu);
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // If e.target == radio btn {
+        // pledgePopOut active
+    //     const radioEl = document.querySelectorAll('.radio-label')
+    // if (e.target == radioEl) {
