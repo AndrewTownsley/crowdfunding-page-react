2fd6de0a8eb844504b65e3033e8b9f49d5565edc
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-23 11:12:30 -0600

    Pledge dollar amount and pledge backers is being updated.  Formatting is needed to display the numbers correctly

--- app.js
+++ app.js
@@ -15,69 +15,71 @@ const pledge200 = document.getElementById('200');
 
 //==============================================================================
 
-// const progressFill = document.getElementById('progress-fill');
-// const btnContainer = document.querySelector('.btn-container');
-// const body = document.querySelector('body');
-// const goalContainer = document.querySelector('.goal-container');
-// const pledgeBtns = document.querySelectorAll('.btn-container .pledge-btn');
-// let newWidth = Number(localStorage.getItem('newWidthStored'))
-// const optionBtns = document.querySelectorAll('.button-options .option-btn');
-// progressFill.style.width = newWidth + "px"; 
+const progressFill = document.getElementById('progress-fill');
+const btnContainer = document.querySelector('.btn-container');
+const body = document.querySelector('body');
+const fundDataContainer = document.getElementById('.fund-data');
+const pledgeBtns = document.querySelectorAll('.pledge-select .pledge-btn');
+console.log(pledgeBtns);
+const pledgeCount = document.getElementById('pledgeCount');
+let newWidth = Number(localStorage.getItem('newWidthStored'))
+progressFill.style.width = newWidth + "px"; 
 
-// let pledgeTotal = 0;
-// let backersCount = 345;
-// let pledgeCounter = parseInt(0);
-// let pledgeCountValue = Number(localStorage.getItem('pledgeValue'));
-// pledgeCount.innerText = `${pledgeCountValue} of`;
+let pledgeTotal = 89914;
+let backersCount = 5007;
+let pledgeCounter = parseInt(0);
+let pledgeCountValue = Number(localStorage.getItem('pledgeValue'));
+pledgeCount.innerText = `${pledgeCountValue} of`;
 
 
-// const updatePledgeTotal = (e) => {
-//     const activePledge = document.querySelector('.custom-pledge-form.active')
-//     if(e.target.nextSibling.nextSibling === activePledge){
-//             activePledge.classList.toggle('active');
-//         } else {
-        
-//                 e.target.nextSibling.nextSibling.classList.toggle('active');
-//                 // customPledgeForm.classList.toggle('active');
-//                 // ---------------------progress bar 
-//                 const progressFill = document.getElementById('progress-fill');
-//                 let pledgeValue = parseInt(e.target.dataset.value);
-//                 progressFill.style.width = parseInt(pledgeTotal);
-//                 let newWidth = pledgeTotal += pledgeValue;
-//                 progressFill.style.width = newWidth + "px";   
-//                 localStorage.setItem('newWidthStored', newWidth)
-//                 //-------------pledge count & backer count -----------------------
-//                 let backersCountDisplay = document.getElementById('backersCount');
-//                 backersCount++;
-//                 backersCountDisplay.innerText = backersCount; 
-//                 const pledgeCount = document.getElementById('pledgeCount');
-//                 let pledgeBtnValue =  parseInt(e.target.dataset.value)
-//                 let pledgeCountValue = pledgeCounter += pledgeBtnValue; 
-//                 pledgeCount.innerText = `${pledgeCountValue} of`;
-//                 localStorage.setItem("pledgeValue", pledgeCountValue);
-//                 console.log(pledgeCountValue);
-//                 if(pledgeCountValue >= 500) {
-//                         btnContainer.classList.add('disabled');
-//                         goalContainer.classList.add('complete');
-//                     }
-//                     }
-//                 }
+const updatePledgeTotal = (e) => {
+    // const activePledge = document.querySelector('.custom-pledge-form.active')
+    // if(e.target.nextSibling.nextSibling === activePledge){
+    //         activePledge.classList.toggle('active');
+    //     } else {
+        e.preventDefault();
+                console.log('Update Pledge Called');
+                e.target.nextSibling.nextSibling.classList.toggle('active');
+                // customPledgeForm.classList.toggle('active');
+                // ---------------------progress bar 
+                const progressFill = document.getElementById('progress-fill');
+                let pledgeValue = parseInt(e.target.dataset.value);
+                progressFill.style.width = parseInt(pledgeTotal);
+                let newWidth = pledgeTotal += pledgeValue;
+                progressFill.style.width = newWidth + "px";   
+                localStorage.setItem('newWidthStored', newWidth)
+                //-------------pledge count & backer count -----------------------
+                let backersCountDisplay = document.getElementById('backersCount');
+                backersCount++;
+                backersCountDisplay.innerText = backersCount; 
+                const pledgeCount = document.getElementById('pledgeCount');
+                let pledgeBtnValue =  parseInt(e.target.dataset.value)
+                let pledgeCountValue = pledgeCounter += pledgeBtnValue; 
+                pledgeCount.innerText = `${pledgeCountValue} of`;
+                localStorage.setItem("pledgeValue", pledgeCountValue);
+                console.log(pledgeCountValue);
+                // if(pledgeCountValue >= 500) {
+                //         btnContainer.classList.add('disabled');
+                //         fundDataContainer.classList.add('complete');
+                //     }
+                    }
+                // }
             
-//             const optionClear = () => {
-//                     const option = document.querySelector('.button-options');
-//                     console.log(option);
-//                     if(option.classList.contains('selected')) {
-//                             e.target.classList.remove('active')
-//                             e.target.parentElement.parentElement.classList.remove('selected');
-//                         } else {
-//                                 null
-//                             }
-//                         }
+            const optionClear = () => {
+                    const option = document.querySelector('.button-options');
+                    console.log(option);
+                    if(option.classList.contains('selected')) {
+                            e.target.classList.remove('active')
+                            e.target.parentElement.parentElement.classList.remove('selected');
+                        } else {
+                                null
+                            }
+                        }
 
                                                  
-//         pledgeBtns.forEach(pledge => {
-//             pledge.addEventListener('mousedown', updatePledgeTotal)
-//                 })
+        pledgeBtns.forEach(pledge => {
+            pledge.addEventListener('click', updatePledgeTotal)
+                })
 
 //==============================================================================
 
