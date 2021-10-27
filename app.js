9bb8020957f4d34f07f3da6f16aad265ce536b8a
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-26 21:20:43 -0600

    Progress bar is being filled when a pledge is made.

--- app.js
+++ app.js
@@ -18,29 +18,13 @@ const btnContainer = document.querySelector('.btn-container');
 const body = document.querySelector('body');
 const fundDataContainer = document.getElementById('.fund-data');
 const pledgeBtns = document.querySelectorAll('.pledge-select .pledge-btn');
-console.log(pledgeBtns);
 const pledgeCount = document.getElementById('pledgeCount');
 
-// const progressFillDisplay = () => {
-//     console.log('progress fun called');
-//     if(newWidth > 89) {
-//         let newWidth = Number(localStorage.getItem('newWidthStored'))
-//     } else {
-//         let newWidth = 89;
-//     }
-//     progressFill.style.width = newWidth + "px"; 
-// }
-
-
-let newWidth = Number(localStorage.getItem('newWidthStored'))
-
-// pledgeCount.innerText = 89914;
 let pledgeTotal = 89914;
 let backersCount = 5007;
 let pledgeCounter = parseInt(89914);
 let pledgeCountValue = Number(localStorage.getItem('pledgeValue'));
 const pledgeCountDisplay = () => {
-    console.log('display func called');
     if (pledgeCountValue > 89914) {
         return pledgeCount.innerText = `${pledgeCountValue} of`;
     } else {
@@ -48,30 +32,29 @@ const pledgeCountDisplay = () => {
     }
 }
 pledgeCountDisplay();
-    
 
+// const progressFillDisplay = () => {
+//     if(newWidth > 89) {
+        let newWidth = Number(localStorage.getItem('newWidthStored'))
+        //     } else {
+            //         let newWidth = 89;
+            //     }
+            //     progressFill.style.width = newWidth + "px"; 
+            // }
 
 const updatePledgeTotal = (e) => {
-    // const activePledge = document.querySelector('.custom-pledge-form.active')
-    // if(e.target.nextSibling.nextSibling === activePledge){
-    //         activePledge.classList.toggle('active');
-    //     } else {
+
         e.preventDefault();
                 e.target.nextSibling.nextSibling.classList.toggle('active');
-                // customPledgeForm.classList.toggle('active');
-                // ---------------------progress bar 
-
-
-
-
+            
                 // Divide both the 100,000 and 89914 by 1000 to get values of 100(%) and the percentage of that number that 89914 is.  
                 const progressFill = document.getElementById('progress-fill');
                 let pledgeValue = parseInt(e.target.dataset.value);
                 progressFill.style.width = parseInt(pledgeTotal);
-                let newWidth = pledgeTotal += pledgeValue;
+                let newWidth = ((pledgeTotal += pledgeValue) / 10000);
                 console.log(`pledgeTotal = ${pledgeTotal}`);
                 console.log(`pledgeValue = ${pledgeValue}`);
-                console.log(newWidth);
+                console.log("newWidth: ",newWidth);
                 progressFill.style.width = newWidth + "px";   
                 localStorage.setItem('newWidthStored', newWidth)
                 //-------------pledge count & backer count -----------------------
@@ -90,7 +73,6 @@ const updatePledgeTotal = (e) => {
             
             const optionClear = () => {
                     const option = document.querySelector('.button-options');
-                    console.log(option);
                     if(option.classList.contains('selected')) {
                             e.target.classList.remove('active')
                             e.target.parentElement.parentElement.classList.remove('selected');
@@ -138,7 +120,6 @@ const toggleThankModal = () => {
 
 const toggleBookMark = () => {
     bookmarkIcon.classList.toggle('active');
-    console.log('bookmark');
 }
 
 
